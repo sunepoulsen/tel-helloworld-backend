30025131e205e743d3169e6e9c34835734b8cf15
sunepoulsen <sunepoulsen@gmail.com>
2020-06-28 16:37:42 +0200

    TEL-7: Add extra test cases.

--- component-tests/src/test/groovy/dk/sunepoulsen/tech/enterprise/labs/helloworld/ct/GreetingsSpec.groovy
+++ component-tests/src/test/groovy/dk/sunepoulsen/tech/enterprise/labs/helloworld/ct/GreetingsSpec.groovy
@@ -38,4 +38,62 @@ class GreetingsSpec extends DeploymentSpecification {
                 'message': 'Hello World'
             ]
     }
+
+    void "GET /bad-request returns 400"() {
+        given: 'HelloWorld service is available'
+            deployment.waitForAvailable(CONTAINER_NAME)
+
+        when: 'Call GET /bad-request'
+            HttpRequest httpRequest = httpHelper.newRequestBuilder(CONTAINER_NAME, '/bad-request')
+                .GET()
+                .build()
+
+            HttpResponseVerificator verificator = httpHelper.sendRequest(httpRequest)
+
+        then: 'Response Code is 400'
+            verificator.responseCode(400)
+
+        and: 'Content Type is application/json'
+            verificator.contentTypeIsJson()
+
+        and: 'Response body is json'
+            verificator.bodyIsJson()
+
+        and: 'Verify greetings body'
+            verificator.bodyAsJson() == [
+                'code': 'bad-request',
+                'param': 'id',
+                'message': 'Invalid id'
+            ]
+    }
+
+    void "GET /unknown-path returns 404"() {
+        given: 'HelloWorld service is available'
+            deployment.waitForAvailable(CONTAINER_NAME)
+
+        when: 'Call GET /unknown-path'
+            HttpRequest httpRequest = httpHelper.newRequestBuilder(CONTAINER_NAME, '/unknown-path')
+                .GET()
+                .build()
+
+            HttpResponseVerificator verificator = httpHelper.sendRequest(httpRequest)
+
+        then: 'Response Code is 404'
+            verificator.responseCode(404)
+
+        and: 'Content Type is application/json'
+            verificator.contentTypeIsJson()
+
+        and: 'Response body is json'
+            verificator.bodyIsJson()
+
+        and: 'Verify greetings body'
+            verificator.bodyAsJson() == [
+                'timestamp': verificator.bodyAsJson().timestamp,
+                'status': 404,
+                'error': 'Not Found',
+                'message': '',
+                'path': '/unknown-path'
+            ]
+    }
 }
