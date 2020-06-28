472b1e8e7984a3d29d8148f543f27a928167f6a2
sunepoulsen <sunepoulsen@gmail.com>
2020-06-28 14:28:50 +0200

    TEL-7: Replaced boiler plates.

--- component-tests/src/test/groovy/dk/sunepoulsen/tech/enterprise/labs/helloworld/ct/GreetingsSpec.groovy
+++ component-tests/src/test/groovy/dk/sunepoulsen/tech/enterprise/labs/helloworld/ct/GreetingsSpec.groovy
@@ -1,36 +1,41 @@
 package dk.sunepoulsen.tech.enterprise.labs.helloworld.ct
 
-import dk.sunepoulsen.tech.enterprise.labs.core.component.tests.docker.DockerDeployment
-import dk.sunepoulsen.tech.enterprise.labs.core.rs.client.TechEnterpriseLabsClient
-import dk.sunepoulsen.tech.enterprise.labs.helloworld.rs.client.HelloWorldIntegrator
-import dk.sunepoulsen.tech.enterprise.labs.helloworld.rs.client.model.Greetings
-import spock.lang.Specification
+import dk.sunepoulsen.tech.enterprise.labs.core.component.tests.specification.DeploymentSpecification
+import dk.sunepoulsen.tech.enterprise.labs.core.component.tests.verification.HttpResponseVerificator
 
-class GreetingsSpec extends Specification {
+import java.net.http.HttpRequest
+
+class GreetingsSpec extends DeploymentSpecification {
 
     private static String CONTAINER_NAME = 'tel-helloworld-service'
-    private static DockerDeployment deployment
 
     void setupSpec() {
-        deployment = new DockerDeployment('ct', [CONTAINER_NAME])
-        deployment.deploy()
-        deployment.waitForAvailable()
-    }
-
-    void cleanupSpec() {
-        deployment.undeploy()
+        initDeployment('ct', [CONTAINER_NAME])
     }
 
     void "GET /greetings returns OK"() {
         given: 'HelloWorld service is available'
             deployment.waitForAvailable(CONTAINER_NAME)
-            TechEnterpriseLabsClient client = deployment.createClient(CONTAINER_NAME)
-            HelloWorldIntegrator sut = new HelloWorldIntegrator(client)
 
         when: 'Call GET /greetings'
-            Greetings greetings = sut.greetings().blockingGet()
+            HttpRequest httpRequest = httpHelper.newRequestBuilder(CONTAINER_NAME, '/greetings')
+                .GET()
+                .build()
 
-        then: 'Verify greetings message'
-            greetings == new Greetings(message: 'Hello World')
+            HttpResponseVerificator verificator = httpHelper.sendRequest(httpRequest)
+
+        then: 'Response Code is 200'
+            verificator.responseCode(200)
+
+        and: 'Content Type is application/json'
+            verificator.contentTypeIsJson()
+
+        and: 'Response body is json'
+            verificator.bodyIsJson()
+
+        and: 'Verify greetings body'
+            verificator.bodyAsJson() == [
+                'message': 'Hello World'
+            ]
     }
 }
